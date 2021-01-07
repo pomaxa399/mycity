64223f5c34d3f8a0030819323f6238ca84850850
pomaxa399 <pomaxa399@gmail.com>
2021-01-07 03:07:33 +0300

    Register models Category and SubCategory in AdminPanel

--- categories/admin.py
+++ categories/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import CategoryCompany, SubCategoryCompany
 
-# Register your models here.
+
+class CategoryCompanyAdmin(admin.ModelAdmin):
+    pass
+
+
+class SubCategoryCompanyAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(CategoryCompany, CategoryCompanyAdmin)
+admin.site.register(SubCategoryCompany, SubCategoryCompanyAdmin)
