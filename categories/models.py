0785ce179a4330b0b33e21f62bf257092b070684
pomaxa399 <pomaxa399@gmail.com>
2021-01-07 03:07:11 +0300

    Add models Category and SubCategory

--- categories/models.py
+++ categories/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class CategoryCompany(models.Model):
+    name = models.CharField('Категория', max_length=150)
+    description = models.TextField('Описание', blank=True)
+    url = models.SlugField(max_length=160, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Категория компании'
+        verbose_name_plural = 'Категории компаний'
+
+
+class SubCategoryCompany(models.Model):
+    name = models.CharField('Подкатегория', max_length=150)
+    description = models.TextField('Описание', blank=True)
+    url = models.SlugField(max_length=160, unique=True)
+    category = models.ForeignKey(CategoryCompany, on_delete=models.CASCADE, related_name='subcategory')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Подкатегория компании'
+        verbose_name_plural = 'Подкатегории компаний'
