d774139c556549ef3dcb0ccf4bdc0a5efef6fd70
pomaxa399 <pomaxa399@gmail.com>
2021-01-07 15:12:38 +0300

    Add models in models companies, register models in django-admin

--- companies/admin.py
+++ companies/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Company, Filial, Foto
 
-# Register your models here.
+
+class CompanyAdmin(admin.ModelAdmin):
+    pass
+
+
+class FilialAdmin(admin.ModelAdmin):
+    pass
+
+
+class FotoAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Company, CompanyAdmin)
+admin.site.register(Filial, FilialAdmin)
+admin.site.register(Foto, FotoAdmin)
--- companies/models.py
+++ companies/models.py
@@ -1,3 +1,52 @@
 from django.db import models
+from categories.models import CategoryCompany, SubCategoryCompany
 
-# Create your models here.
+
+class Company(models.Model):
+    name = models.CharField('Название', max_length=160)
+    description = models.TextField('Описание', blank=True)
+    logo = models.ImageField(verbose_name='Логотип', blank=True)
+    advantage = models.CharField('Преимущества', max_length=250)
+    url = models.SlugField(max_length=170, unique=True)
+    frame_in = models.CharField('Фрейм внутри', max_length=250, blank=True)
+    frame_out = models.CharField('Фрейм снаружи', max_length=250, blank=True)
+    category = models.ManyToManyField(CategoryCompany, related_name='category_company')
+    subcategory = models.ManyToManyField(SubCategoryCompany, related_name='subcategory_company')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Компания'
+        verbose_name_plural = 'Компании'
+
+
+class Filial(models.Model):
+    frame_in = models.CharField('Фрейм внутри', max_length=250, blank=True)
+    frame_out = models.CharField('Фрейм снаружи', max_length=250, blank=True)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE,
+                                related_name='filial_company')
+
+    def __str__(self):
+        return self.company.name
+
+    class Meta:
+        verbose_name = 'Филиал'
+        verbose_name_plural = 'Филиалы'
+
+
+class Foto(models.Model):
+    name = models.CharField('Название', max_length=150)
+    description = models.TextField('Описание')
+    image = models.ImageField('Изображение', upload_to='company_foto/')
+    company = models.ForeignKey(Company, on_delete=models.CASCADE,
+                                related_name='foto_company', null=True, blank=True)
+    filial = models.ForeignKey(Filial, on_delete=models.CASCADE,
+                               related_name='foto_filial', null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Изображение'
+        verbose_name_plural = 'Изображения'
