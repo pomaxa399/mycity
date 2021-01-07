afe5f72afe15dddbd4b6aa7d86b77fe7ac9abd40
pomaxa399 <pomaxa399@gmail.com>
2021-01-07 03:06:41 +0300

    Add app categories in settings installed apps

--- mycity/settings.py
+++ mycity/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'companies.apps.CompaniesConfig',
+    'categories.apps.CategoriesConfig',
 ]
 
 MIDDLEWARE = [
